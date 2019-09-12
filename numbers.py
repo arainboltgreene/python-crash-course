f6259d0fe795b4656ddecb8b7f7c6a4c585c4580
Angela Rainbolt-Greene <angelaenglund@gmail.com>
2019-09-12 00:31:47 -0700

    ugh

---
+++ numbers.py
@@ -0,0 +1,10 @@
+# 4-3 counting to twenty. use for loop to count from 1 to 20,
+# inclusive
+
+for value in range(1,21):
+    print(value)
+
+# 4-4 one million
+
+numbers = list(range(1,1000001))
+print(numbers)
