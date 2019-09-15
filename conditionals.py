4c547cf7bcaa4c88712ca74c823a56ccbfa668f0
Angela Rainbolt-Greene <angelaenglund@gmail.com>
2019-09-14 19:54:11 -0700

    moar commit

---
+++ conditionals.py
@@ -0,0 +1,31 @@
+# 5-1 Conditional tests
+
+# true
+doll = 'owned'
+print("is doll == 'owned'? i predict true")
+print(doll == 'owned')
+# true
+drinker = 21
+print("can drinker legally drink? i predict true")
+print(drinker == 21)
+# true
+cat = ['happy', 'healthy']
+print("is cat happy OR healthy? i predict true")
+print('happy' in cat) or ('healthy' in cat)
+toppings = ['whipped cream', 'sprinkles', 'gummy bears']
+'sprinkles' in toppings
+# true
+
+# true
+
+# false
+keys = 'notowned'
+print("is keys == 'owned'? I predict false")
+print(keys == 'owned')
+# false
+cat = ['happy', 'healthy']
+print("is cat happy AND healthy? i predict false")
+print(cat == 'happy') and (cat == 'unhealthy')
+# false
+# false
+# false
