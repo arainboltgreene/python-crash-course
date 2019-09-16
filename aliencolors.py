9c40d43267437cfa02cae6cda10c7d452d5cec4f
A. Rainbolt-Greene <angelaenglund@gmail.com>
2019-09-15 22:51:13 -0700

    yet another commit message

---
+++ aliencolors.py
@@ -0,0 +1,31 @@
+# 5-3 alien colors #1
+
+alien_colors = ['green']
+
+if 'green' in alien_colors:
+    print("You just earned 5 points!")
+
+if 'blue' in alien_colors:
+    print("Faaaail")
+
+# 5-4 alien colors #2
+print("\nthis one is an if chain")
+
+alien_colors2 = ['green']
+
+if 'green' in alien_colors2:
+    print("You just earned 5 points!")
+if 'green' in alien_colors2:
+    print("You just earned 10 points")
+
+print("\nthis one is an if block")
+
+alien_colors3 = ['green']
+
+if 'green' in alien_colors3:
+    print("you just earned 5 points")
+elif 'green' in alien_colors3:
+    print("you just earned 10 points")
+
+# 5-5 alien colors #3
+
