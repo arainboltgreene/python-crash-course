8bf32126025f426480beb071d1dea77cb76cad2e
A. Rainbolt-Greene <angelaenglund@gmail.com>
2019-09-22 18:27:17 -0700

    deleted empty/unused files. renamed files in order i did them in the book.

--- math.py
+++
@@ -1,6 +0,0 @@
-print(5 + 3)
-print(2 * 4)
-print(16 / 2)
-# this is how you write a comment
-fav = 9
-print("My favorite number is " + str(fav))
--- recursion.py
+++
@@ -1,8 +0,0 @@
-def recursion(level):
-    if level == 100:
-        print("done")
-    else:
-        print("my level is %s" % level)
-        recursion(level+1)
-
-recursion(1)
