c0ae3d487106852096fc2ab229438b65202a856b
A. Rainbolt-Greene <angelaenglund@gmail.com>
2019-10-04 19:05:40 -0700

    new stuff

---
+++ 015dicts3.py
@@ -0,0 +1,13 @@
+# 6-7 Person redeux
+
+mike = {'first_name': 'mike', 'last_name': 'mcmurry', 'age': '42', 'city': 'south beach'}
+lyla = {'first_name': 'lyla', 'last_name': 'mchyrtz', 'age': '30', 'city': 'jacksonville'}
+adrian = {'first_name': 'adrian', 'last_name': 'mcmannis', 'age': '17', 'city': 'nowhere'}
+
+people = [mike, lyla, adrian]
+
+
+
+
+for person in people:
+    print(person)
