c9642733b9073970744da7500b1393a05c2df051
A. Rainbolt-Greene <angelaenglund@gmail.com>
2020-01-27 13:01:37 -0800

    8-6

--- 021returnvalues.py
+++ 021returnvalues.py
@@ -1,75 +1,19 @@
-print("Making an argument optional:")
+# 8-6 City Names:
+#print("\n Try It Yourself section")
 
-def get_formatted_name(first_name, last_name):
-    """Return a full name, neatly formatted."""
-    full_name = first_name + ' ' + last_name
-    return full_name.title()
+#def city_country(city_name, country_name):
+#    place = city_name + ' ' + country_name
+#    return place.title()
 
-musician = get_formatted_name('jimi', 'hendrix')
-print(musician)
+#citycountry = get_formatted_name('Santiago, Chile')
+# citycountry = get_formatted_name('Puente Alto, Chile')
+# citycountry = get_formatted_name('Antofagasta, Chile')
+#print(citycountry)
 
-print("\nMaking an argument optional part 1:")
-def get_formatted_name(first_name, middle_name, last_name):
-    """Return a full name, neatly formatted."""
-    full_name = first_name + ' ' + middle_name + ' ' + last_name
-    return full_name.title()
+# 8-7 Album
+def make_album(artist_name, album_title):
+    album1 = {'name': artist_name, 'title': album_title}
+    return album1
 
-musician = get_formatted_name('john', 'lee', 'hooker')
-print(musician)
-
-print("\nMaking an argument optional part 2:")
-def get_formatted_name(first_name, last_name, middle_name=''):
-    """Return a full name, neatly formatted."""
-    if middle_name:
-        full_name = first_name + ' ' + middle_name + ' ' + last_name
-    else:
-        full_name = first_name + ' ' + last_name
-    return full_name.title()
-
-musician = get_formatted_name('jimi', 'hendrix')
-print(musician)
-
-musician = get_formatted_name('john', 'hooker', 'lee')
-print(musician)
-
-print("\nReturning a Dictionary:")
-
-def build_person(first_name, last_name):
-    """Return a dictionary of information about a person"""
-    person = {'first': first_name, 'last': last_name}
-    return person
-
-musician = build_person('jimi', 'hendrix')
-print(musician)
-
-def build_person(first_name, last_name, age=''):
-    """Return a dictionary of information about a person."""
-    person = {'first': first_name, 'last': last_name}
-    if age:
-        person['age'] = age
-    return person
-
-musician = build_person('jimi', 'hendrix', age=27)
-print(musician)
-
-print("\nUsing a function with a while loop")
-def get_formatted_name(first_name, last_name):
-    """Return a full name, neatly formatted"""
-    full_name = first_name + ' ' + last_name
-    return full_name.title()
-
-# This is an infinite loop
-while True:
-    print("\nPlease tell me your name:")
-    print("(enter 'q' at any time to quit)")
-
-    f_name = input("First name: ")
-    if f_name == 'q':
-        break
-
-    l_name = input("Last name: ")
-    if l_name == 'q':
-        break
-
-    formatted_name = get_formatted_name(f_name, l_name)
-    print("\nHello, " + formatted_name + "!")
+album = make_album('jimi hendrix', 'axis of love')
+print(album)
